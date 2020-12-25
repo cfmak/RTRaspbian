5be05c1e8a2d37df8fbeabb036cd9285773e0b7d
Geoffrey Mak <geoffreymak.000@gmail.com>
2020-12-24 23:05:36 -0600

    setup.sh add git config; disable build because we need to edit .config file

--- setup.sh
+++ setup.sh
@@ -1,7 +1,12 @@
 sudo apt install emacs-nox -y
 sudo apt install git -y
 
-sudo apt install bc bison flex libssl-dev make libc6-dev libncurses5-dev crossbuild-essential-arm64
+sudo apt install bc bison flex libssl-dev make libc6-dev libncurses5-dev crossbuild-essential-arm64 -y
 
-
-
+git config --global user.email "geoffreymak.000@gmail.com"
+git config --global user.name "Geoffrey Mak"
+git config --global core.editor emacs
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global alias.ci commit
+git config --global alias.st status
