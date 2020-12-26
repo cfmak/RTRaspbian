478fe2c162b962552d75fddf4ffab2833fc833f4
Geoffrey Mak <geoffreymak.000@gmail.com>
2020-12-25 18:43:04 -0600

    copy file to target directory

--- fetch-patch-build.sh
+++ fetch-patch-build.sh
@@ -16,4 +16,18 @@ make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- bcm2711_defconfig
 
 cat ../config.patch | patch .config
 
-#make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- Image modules dtbs
+make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- Image modules dtbs
+
+FAT32_TARGET=../target/mnt/fat32
+EXT4_TARGET=../target/mnt/ext4
+mkdir -p ${FAT32_TARGET}
+mkdir -p ${FAT32_TARGET}/overlays
+mkdir -p ${EXT4_TARGET}
+
+sudo env PATH=$PATH make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- INSTALL_MOD_PATH=${EXT4_TARGET} modules_install
+
+KERNEL=kernel8
+sudo cp arch/arm64/boot/Image ${FAT32_TARGET}/$KERNEL.img
+sudo cp arch/arm64/boot/dts/broadcom/*.dtb ${FAT32_TARGET}
+sudo cp arch/arm64/boot/dts/overlays/*.dtb* ${FAT32_TARGET}/overlays/
+sudo cp arch/arm64/boot/dts/overlays/README ${FAT32_TARGET}/overlays/
